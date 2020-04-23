f77b762ad04f84635a0530cb416cf64730a05b3a
rochusqwerty <rochus96@gmail.com>
2020-04-23 10:02:48 +0200

    Tests for sub-accounts

---
+++ OOPBank.Tests/DepositAccountTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+
+namespace OOPBank.Tests
+{
+    [TestFixture]
+    class DepositAccountTest
+    {
+        private readonly DepositAccount DepositAccount1;
+        private readonly Customer SomeCustomer;
+        private Mock<Account> Account1;
+        private Mock<Account> Account2;
+        private Mock<Operation> mockOp;
+
+        public DepositAccountTest()
+        {
+            SomeCustomer = new Customer("", "");
+            DepositAccount1 = new DepositAccount(SomeCustomer, "A01", new Money(100),
+                new Money(100), 1);
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            Account1 = new Mock<Account>();
+            Account1.Setup(t => t.accountNumber).Returns("1");
+
+            Account2 = new Mock<Account>();
+            Account2.Setup(t => t.accountNumber).Returns("2");
+
+            mockOp = new Mock<Operation>();
+            mockOp.Setup(t => t.DateOfExecution).Returns(new DateTime(2020, 04, 01));
+            mockOp.Setup(t => t.FromAccount).Returns(Account1.Object);
+            mockOp.Setup(t => t.ToAccount).Returns(Account2.Object);
+            mockOp.Setup(t => t.Money).Returns(new Money(100));
+        }
+
+        [Test]
+        public void ConstructorExceptionTest()
+        {
+            Assert.Throws(Is.TypeOf<Exception>()
+                .And.Message.EqualTo("Deposit duration has to be longer than 0 days."), () =>
+                new DepositAccount(SomeCustomer, "A01", new Money(),
+                    new Money(0, 1), 0));
+            Assert.Throws(Is.TypeOf<Exception>()
+                .And.Message.EqualTo("Deposit amount has to be greater than 0."), () => new DepositAccount(SomeCustomer,
+                "A01", new Money(),
+                new Money(), 1));
+        }
+
+        [Test]
+        public void DepositOperationsTest()
+        {
+            DepositAccount1.bookIncomingOperation(mockOp.Object);
+            Assert.AreEqual(200, DepositAccount1.getBalance().asDouble);
+            DepositAccount1.bookOutgoingOperation(mockOp.Object);
+            DepositAccount1.bookOutgoingOperation(mockOp.Object);
+            Assert.AreEqual(0, DepositAccount1.getBalance().asDouble);
+            DepositAccount1.bookOutgoingOperation(mockOp.Object);
+            Assert.AreEqual(0, DepositAccount1.getBalance().asDouble);
+        }
+
+        [Test]
+        public void AccountTest()
+        {
+            Assert.IsTrue(DepositAccount1.hasSufficientBalance(new Money(200)));
+            Assert.IsFalse(DepositAccount1.hasSufficientBalance(new Money(200, 1)));
+        }
+    }
+}
---
+++ OOPBank.Tests/LoanAccountTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+
+namespace OOPBank.Tests
+{
+    [TestFixture]
+    class LoanAccountTest
+    {
+        private Mock<Account> Account1;
+        private Mock<Account> Account2;
+        private readonly LoanAccount LoanAccount1;
+        private Mock<Operation> mockOp;
+        private readonly Customer SomeCustomer;
+
+        public LoanAccountTest()
+        {
+            SomeCustomer = new Customer("", "");
+            LoanAccount1 = new LoanAccount(SomeCustomer, "A01", new Money(100), new Money(100));
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            Account1 = new Mock<Account>();
+            Account1.Setup(t => t.accountNumber).Returns("1");
+
+            Account2 = new Mock<Account>();
+            Account2.Setup(t => t.accountNumber).Returns("2");
+
+            mockOp = new Mock<Operation>();
+            mockOp.Setup(t => t.DateOfExecution).Returns(new DateTime(2020, 04, 01));
+            mockOp.Setup(t => t.FromAccount).Returns(Account1.Object);
+            mockOp.Setup(t => t.ToAccount).Returns(Account2.Object);
+            mockOp.Setup(t => t.Money).Returns(new Money(100));
+        }
+
+        [Test]
+        public void ConstructorExceptionTest()
+        {
+            Assert.Throws(Is.TypeOf<Exception>()
+                    .And.Message.EqualTo("Loan amount has to be greater than 0."),
+                () => new LoanAccount(SomeCustomer, "A01", new Money(100), new Money()));
+        }
+
+        [Test]
+        public void LoanOperationsTest()
+        {
+            LoanAccount1.bookInstallmentOperation(mockOp.Object);
+            Assert.AreEqual(0, LoanAccount1.getBalance().asDouble);
+            Assert.AreEqual(0, LoanAccount1.loanAmount.asDouble);
+        }
+
+        [Test]
+        public void AccountTest()
+        {
+            Assert.IsTrue(LoanAccount1.tooMuchTransfer(new Money(100, 1)));
+            Assert.IsFalse(LoanAccount1.tooMuchTransfer(new Money(100)));
+        }
+    }
+}
