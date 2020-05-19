07d5030899b9fbf6872f37298aa4d709858b0475
tomasz-pucka <tomasz.pucka2@gmail.com>
2020-05-19 19:22:27 +0200

    add State design pattern for InterestRate

--- OOPBank/Classes/Account.cs
+++ OOPBank/Classes/Account.cs
@@ -1,20 +1,16 @@
 using System.Collections.Generic;
-using OOPBank.Classes;
 
 namespace OOPBank
 {
     public class Account
     {
-        public virtual string accountNumber { get; }
-        public virtual List<Operation> IncomingOperations { get; } = new List<Operation>();
-        public virtual List<Operation> OutgoingOperations { get; } = new List<Operation>();
-
-        public Account()
-        {
-        }
         public Account(string number)
         {
             accountNumber = number;
         }
+
+        public string accountNumber { get; }
+        public List<Operation> IncomingOperations { get; } = new List<Operation>();
+        public List<Operation> OutgoingOperations { get; } = new List<Operation>();
     }
 }
---
+++ OOPBank/InterestRate/InterestRateRich.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOPBank.InterestRate
+{
+    public class InterestRateRich : InterestRate
+    {
+        private const double BalanceConstant = 5;
+
+        public InterestRateRich(LocalAccount account, List<Operation> incomingOperations,
+            List<Operation> outgoingOperations) : base(account, incomingOperations, outgoingOperations)
+        {
+        }
+
+        public override double calculateInterest(Action<InterestRate> setInterestRateState)
+        {
+            var amount = (BalanceConstant + Math.Log2(incomingOperations.Count) + Math.Log2(outgoingOperations.Count)) *
+                         0.01;
+            if (amount < 0.005) amount = 0.005;
+            else if (amount > 0.2) amount = 0.2;
+            if (account.getBalance() < 100000)
+                setInterestRateState(new InterestRatePoor(account, incomingOperations, outgoingOperations));
+            return amount;
+        }
+    }
+}
