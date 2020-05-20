11e3b56e95a470555b28589b9cfef3cefc16111d
tomasz-pucka <tomasz.pucka2@gmail.com>
2020-05-20 02:25:59 +0200

    add Decorator design pattern in LocalAccount for withdrawing money, add tests

---
+++ OOPBank/Classes/ILocalAccount.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPBank.Classes
+{
+    public interface ILocalAccount
+    {
+        Money balance { get; set; }
+        void withdrawMoney(Money amount);
+    }
+}
